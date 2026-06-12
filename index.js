8cff955c61f71286f72ff8db3e49729759c2ff17
K-M-Shehan <miweshshehan@gmail.com>
2026-06-12 23:29:22 +0530

    proper grid is made
    grid with styling defined in flex

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const containerDiv = document.querySelector("#container");
 
-let numOfCells = 100;
+let numOfCells = 16;
+let grid = numOfCells * numOfCells;
 
-for (let i = 0; i < numOfCells; i++) {
+for (let i = 0; i < grid; i++) {
   const div = document.createElement("div");
   div.setAttribute("id", "cell");
+  div.style.flex = "0 0 " + (100 / numOfCells) + "%"; // gets the particular column size
   containerDiv.appendChild(div);
   div.addEventListener("mouseover", () => {
     div.style.backgroundColor = "black";
