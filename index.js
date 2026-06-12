43cf09b11b62d91c5231bb63f05b4ca29381e00d
K-M-Shehan <miweshshehan@gmail.com>
2026-06-12 22:56:39 +0530

    add hover event listener to draw
    we can now draw over the cells

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const containerDiv = document.querySelector("#container");
 
-let numOfCells = 16;
+let numOfCells = 100;
 
 for (let i = 0; i < numOfCells; i++) {
   const div = document.createElement("div");
   div.setAttribute("id", "cell");
   containerDiv.appendChild(div);
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "black";
+  })
 }
