4c76f73502b71ce65c83047224d29ca6fc00a328
K-M-Shehan <miweshshehan@gmail.com>
2026-06-12 23:53:07 +0530

    modularize grid creation to function
    also made function for destruction (erasure when config is done) also a new grid is created when configured in prompt

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const containerDiv = document.querySelector("#container");
+const button = document.querySelector("#size-btn");
 
 let numOfCells = 16;
-let grid = numOfCells * numOfCells;
 
-for (let i = 0; i < grid; i++) {
-  const div = document.createElement("div");
-  div.setAttribute("id", "cell");
-  div.style.flex = "0 0 " + (100 / numOfCells) + "%"; // gets the particular column size
-  containerDiv.appendChild(div);
-  div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "black";
-  })
+function drawGrid(cells) {
+  let grid = cells * cells;
+
+  for (let i = 0; i < grid; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("id", "cell");
+    div.style.flex = "0 0 " + (100 / numOfCells) + "%"; // gets the particular column size
+    containerDiv.appendChild(div);
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "black";
+    })
+  } 
 }
+
+function removeGrid(cells) {
+  let grid = cells * cells;
+
+  for (let i = 0; i < grid; i++) {
+    containerDiv.removeChild(document.querySelector("#cell"));
+  } 
+}
+
+drawGrid(numOfCells); // default grid that is drawn
+
+button.addEventListener("click", () => {
+  removeGrid(numOfCells);
+  numOfCells = prompt("Enter new size (size x size)");
+  drawGrid(numOfCells);
+})
